7e83a2bc460271a399c34a6dd8bdf27ef074bc24
Chris Bolas <tanlaan33@gmail.com>
2021-12-15 12:20:52 -0800

    Pull past and future into model scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,16 @@ class Event < ApplicationRecord
   validates :title, :description, :date, :location, presence: true
   belongs_to :creator, class_name: 'User'
   has_and_belongs_to_many :attendees, class_name: 'User'
+
+  scope :past, -> { where('date < ?', Time.now) }
+  scope :future, -> { where('date > ?', Time.now) }
+
+  # def self.past
+  #   where('date < ?', Time.now)
+  # end
+
+  # def self.future
+  #   where('date > ?', Time.now)
+  # end
+
 end
